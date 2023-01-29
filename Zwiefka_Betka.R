5430436b8a453e9d63742b4dc93276c71cf5fb32
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-29 19:57:06 +0100

    cały skrypt

--- Zwiefka_Betka.R
+++ Zwiefka_Betka.R
@@ -1,3 +1,110 @@
+
+# CZĘŚĆ 1 PROJKETU
+#ZADANIE 1
+
+
+library(ggplot2)
+data1 <- read.csv("~/Downloads/zwc_d.csv")
+data2 <-  read.csv("~/Downloads/wwl_d.csv")
+#data1 <- zwc_d <- read.csv("//nas1/home/azwiefka/Downloads/zwc_d.csv")
+#data2 <- read.csv("//nas1/home/azwiefka/Downloads/wwl_d.csv")
+class(data1)
+class(data2)
+
+data1 <- subset(data1,data1$Data %in% data2$Data)
+data2 <- subset(data2,data2$Data %in% data1$Data)
+
+data1_closed <- data1$Zamkniecie
+data1_date <- as.Date(data1$Data,"%Y-%m-%d")
+
+
+ggplot(data = data1, aes(data1_date,data1_closed)) + geom_line()+ggtitle("Wykres")+labs(x="data", y="kursy",main="wykres")+ theme(plot.title = element_text(hjust = 0.5))
+
+hist(data1_closed,prob =TRUE,xlab="kursy", ylab="gęstość",main="histogram") 
+#plotdist(data1_closed, histo = TRUE, demp = TRUE)
+
+#ZAD 2 
+statistics <- descdist(data1_closed,boot=1000)
+
+expected_value <-  statistics$mean
+standard_deviation <- statistics$sd
+
+library(moments)
+#skewness > 0
+# świadczy to o prawostronnej asymetrii rozkładu
+skewness <- statistics$skewness
+
+#możemy powiedzieć, że znaczna część wyników / obserwacji jest
+#podobna do siebie a obserwacji znacznie różniących się od 
+#średniej jest mało
+kurtosis <- statistics$kurtosis
+
+
+df = data.frame(
+  wartosc_oczekiwana=expected_value,
+  odchylenie_standardowe= standard_deviation ,
+  skosność= skewness ,
+  kurtoza = kurtosis
+)
+rownames(df) <- "Akcja"
+df
+
+
+#ZAD 3 ZZAD 4
+
+
+#nie trzeb apodawac method="mle". bo to jest domyślne
+library(fitdistrplus)
+fnorm <- fitdist(data1_closed,"norm")
+flnorm <- fitdist(data1_closed,"lnorm")
+fweibull <- fitdist(data1_closed, "weibull")
+
+
+summary(fnorm)
+summary(flnorm)
+summary(fweibull)
+
+
+par(mfrow=c(2,2))
+plot.legend <- c("Normal","Lognormal","weibull")
+denscomp(list(fnorm,flnorm,fweibull),legendtext= plot.legend)
+qqcomp(list(fnorm,flnorm,fweibull),legendtext= plot.legend)
+cdfcomp(list(fnorm,flnorm,fweibull),legendtext= plot.legend)
+ppcomp(list(fnorm,flnorm,fweibull),legendtext= plot.legend)
+
+gofstat(list(fnorm, flnorm,fgamma),fitnames = c("norm", "lnorm", "weibull"))
+
+#zad 5
+
+N <- 10000
+n <- length(data1_closed); 
+Dln <- c()
+
+for (i in 1:N) { 
+  
+  Yln <- rlnorm(n,flnorm$estimate[1],flnorm$estimate[2])
+  
+  Dln[i] <-  ks.test(Yln,plnorm, flnorm$estimate[1],flnorm$estimate[2],exact=TRUE)$statistic
+}
+
+dn_ln <-  ks.test(data1_closed,plnorm,flnorm$estimate[[1]],flnorm$estimate[[2]],exact=TRUE)$statistic
+dn_ln
+par(mfrow=c(1,1))
+hist(Dln,prob=T,xlim=range(0,0.15))
+points(dn_ln,0,pch=20,col=2)
+
+p_value_ln <- length(Dln[Dln>dn_ln])/N
+
+alpha <- 0.8
+p_value_ln <= alpha
+
+#Wartosc p-value jest mniejsza od przyjetego poziomu istotnosci, 
+p_#zatem hipoteze o rownosci dystrybuant (F=F0, gdzie F poszukiwany rozklad) odrzucamy.
+
+
+
+
+
 #Częsć 2 projektu
 library(ggplot2)
 nike <- read.csv("nke_de_d.csv")
@@ -81,7 +188,7 @@ alpha <- ppoints(n)
 q_emp <- quantile(dM,alpha)
 q_teo <- qchisq(alpha,df=2)
 
-plot(q_emp,q_teo,pch=19)
+plot(q_emp,q_teo,pch=19,title("Wykres diagnostyczny QQ-plot"))
 abline(a=0,b=1,col=2)
 
 #testujemyhipoteze, ze kwadraty odleglosci Mahalanobisa maja rozklad chi(2)
@@ -182,7 +289,7 @@ RSE
 #test istotnosci wspolczynnikow bo, b1
 #Obliczamy wartosci statystyki T oraz p-value
 #wspolczynniki modelu 
-#### BŁĄD!!!!!!
+
 coef <- log_zwroty_lm$coefficients  
 beta0 <- coef[[1]]
 beta1 <- coef[[2]]
@@ -208,8 +315,8 @@ t0; t1
 # czy 1f)
 #PREDYKCJA
 m <- mean(log_zwroty_nike)
-
-beta0+beta1*m # czy to jest model 2?
+m
+beta0+beta1*m 
 #wyniki predykcji -0.0002776217
 
 #Predykcja i przedzialy ufnosci dla predykcji
